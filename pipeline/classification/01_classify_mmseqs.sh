a6bd8e4ec6e13fc101d1abe2ebfe07d0577b18ec
Jason Stajich <jasonstajich.phd@gmail.com>
2025-04-18 10:06:27 -0700

    dump telo

---
+++ pipeline/classification/01_classify_mmseqs.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -N 1 -c 64 -C ryzen -n 1 --mem 255gb --out logs/mmseqs_classify.%a.log -J classify
+
+CPU=2
+if [ $SLURM_CPUS_ON_NODE ]; then
+  CPU=$SLURM_CPUS_ON_NODE
+fi
+
+module load mmseqs2
+module load workspace/scratch
+SAMPFILE=samples.csv
+N=${SLURM_ARRAY_TASK_ID}
+if [ -z $N ]; then
+  N=$1
+fi
+if [ -z $N ]; then
+  echo "cannot run without a number provided either cmdline or --array in sbatch"
+  exit
+fi
+DB=/srv/projects/db/ncbi/mmseqs/uniref50
+#DB=/srv/projects/db/ncbi/mmseqs/swissprot
+DBNAME=$(basename $DB)
+
+IFS=,
+INPUT=input
+OUTFOLDER=results/classify_mmseqs
+mkdir -p $OUTFOLDER
+
+cat $SAMPFILE | sed -n ${N}p | while read STRAIN
+do
+  PREFIX=$(basename $STRAIN .fasta.gz)
+  mmseqs touchdb $DB
+  GENOME=$INPUT/$STRAIN
+  OUT=$OUTFOLDER/${PREFIX}/${DBNAME}
+  mkdir -p $OUTFOLDER/$PREFIX
+  if [ ! -s ${OUT}_tophit_aln ]; then
+      mmseqs easy-taxonomy $GENOME $DB $OUT $SCRATCH --threads $CPU --lca-ranks kingdom,phylum,family  --tax-lineage 1 --db-load-mode 2
+  fi
+done
--- pipeline/summary/01_telomere_report.sh
+++ pipeline/summary/01_telomere_report.sh
@@ -3,9 +3,9 @@
 
 module load parallel
 
-mkdir -p telomere_reports
+mkdir -p results/telomere_reports
 for a in $(ls input/*.gz)
 do
 	pigz -dc $a > $SCRATCH/$(basename $a .gz)
 done
-ls $SCRATCH/*.fasta | parallel -j 4 python scripts_Hiltunen/find_telomeres.py {} \> telomere_reports/{/.}.telomere_report.txt
+ls $SCRATCH/*.fasta | parallel -j 4 python scripts_Hiltunen/find_telomeres.py {} \> results/telomere_reports/{/.}.telomere_report.txt
